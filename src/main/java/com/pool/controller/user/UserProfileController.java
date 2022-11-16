cc14e189153fec09e55582fac0e9a6a037cfa495
shiva508 <dasarishiva1@gmail.com>
2022-11-16 07:40:35 +0530

    moved to service

--- src/main/java/com/pool/controller/user/UserProfileController.java
+++ src/main/java/com/pool/controller/user/UserProfileController.java
@@ -2,14 +2,12 @@ package com.pool.controller.user;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -20,11 +18,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.pool.entity.UserProfile;
 import com.pool.repository.UserProfileRepository;
+import com.pool.service.UserProfileService;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -33,8 +30,11 @@ public class UserProfileController {
 
 	private final UserProfileRepository userProfileRepository;
 
-	public UserProfileController(UserProfileRepository userProfileRepository) {
+	private final UserProfileService profileService;
+
+	public UserProfileController(UserProfileRepository userProfileRepository, UserProfileService profileService) {
 		this.userProfileRepository = userProfileRepository;
+		this.profileService = profileService;
 	}
 
 	@PostMapping("/save")
@@ -44,29 +44,11 @@ public class UserProfileController {
 
 	@GetMapping("/downloadFile")
 	public ResponseEntity<Mono<Resource>> downloadFile() throws IOException {
-		// Load file as Resource
-
-		Path path = Paths.get(
-				"/home/shiva/shiva/mywork/zero_to_hero_reactive_web/src/main/resources/application.yml");
-		byte[] arr = Files.readAllBytes(path);
-		ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-		arrayOutputStream.write(arr);
-
-		ByteArrayOutputStream arrayOutputStream2 = new ByteArrayOutputStream();
-		ZipOutputStream zipOutputStream = new ZipOutputStream(arrayOutputStream2);
-		ZipEntry zipEntry = new ZipEntry("shiva.yml");
-		zipEntry.setSize(arrayOutputStream.toByteArray().length);
-		zipOutputStream.putNextEntry(zipEntry);
-		zipOutputStream.write(arrayOutputStream.toByteArray());
-		zipOutputStream.closeEntry();
-		zipOutputStream.close();
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(arrayOutputStream2.toByteArray());
-		Resource resource = new InputStreamResource(inputStream);
 
 		return ResponseEntity.ok()
 				.contentType(MediaType.parseMediaType(
 						"application/octet-stream"))
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=appl.zip")
-				.body(Mono.just(resource));
+				.body(profileService.downloadFile());
 	}
 }
--- src/main/java/com/pool/service/UserProfileServiceImpl.java
+++ src/main/java/com/pool/service/UserProfileServiceImpl.java
@@ -1,5 +1,15 @@
 package com.pool.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
@@ -39,8 +49,30 @@ public class UserProfileServiceImpl implements UserProfileService {
 
 	@Override
 	public Mono<Resource> downloadFile() {
-		// TODO Auto-generated method stub
-		return null;
+		Path path = Paths.get(
+				"/home/shiva/shiva/mywork/zero_to_hero_reactive_web/src/main/resources/application.yml");
+		byte[] arr;
+		ByteArrayOutputStream arrayOutputStream = null;
+		Resource resource = null;
+		try {
+			arr = Files.readAllBytes(path);
+			arrayOutputStream = new ByteArrayOutputStream();
+			arrayOutputStream.write(arr);
+			ByteArrayOutputStream arrayOutputStream2 = new ByteArrayOutputStream();
+			ZipOutputStream zipOutputStream = new ZipOutputStream(arrayOutputStream2);
+			ZipEntry zipEntry = new ZipEntry("shiva.yml");
+			zipEntry.setSize(arrayOutputStream.toByteArray().length);
+			zipOutputStream.putNextEntry(zipEntry);
+			zipOutputStream.write(arrayOutputStream.toByteArray());
+			zipOutputStream.closeEntry();
+			zipOutputStream.close();
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(arrayOutputStream2.toByteArray());
+			resource = new InputStreamResource(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Mono.just(resource);
 	}
 
 }
